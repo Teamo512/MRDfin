5ed0f3b11b49527fbbdd1a3c500ad16386196919
Jiang <jiang512bo@163.com>
2017-08-06 18:06:22 +0800

    修改了gitignore文件

--- src/mrDfin/MRDfin_Driver.java
+++ src/mrDfin/MRDfin_Driver.java
@@ -8,7 +8,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Map.Entry;
@@ -68,7 +67,8 @@ public class MRDfin_Driver extends Configured implements Tool{
 		conf.set("mapreduce.task.timeout", "6000000");
 		conf.setInt("mapperNum", mapperNum);
 	
-		useFileCache = false;
+		//useFileCache = false;
+		useFileCache = true;
 		conf.setBoolean("Cache", useFileCache);
 		
 		//命令行的额外参数，必须是成对出现的，分别为配置文件的的属性名和属性值。
